37ea00cb4e0ab799c4b4d03fdae5d48ecfca14bf
Axeleik <axeleik@googlemail.com>
2017-03-17 19:18:36 +0100

    zweiter  push

--- curvature.py
+++ curvature.py
@@ -17,57 +17,92 @@ def grad(curve, scale):
 def length(array,scale):
     size = array.shape[0]
     new = np.zeros((size,2))
-    print "joj:", new
+
     for i in xrange(0,size-1):
         new[i,0]=scale+i
         new[i,1]=np.linalg.norm(array[i+1])
 
     return new
 
+def printname(name):
+     print name
 
-f = h5py.File("/home/axeleik/Downloads/cremi.paths.crop.split_z.h5", mode='r')
 
-curve = np.array(f["z_predict0/truepaths/z/0/beta_0.5/11/0"])[:,:]
+def plot_tf(curve,ok,scale1,scale2,scale3,a,b):
+    path="Truepath"
 
-print curve.shape
-print np.zeros(curve.shape).shape
-print curve.shape[0]
 
-gradi=grad(curve,3)
-print "gradi: \n",gradi
-#print "jojojo:",gradi[2, 1]
-#test=np.zeros((gradi.shape[0],2))
-#print test.shape
-print "jojojo",gradi[1,0],gradi[1,1],gradi[1,2]
-print "hi: ",np.linalg.norm(gradi[1])
-len=length(gradi,3)
-print len
 
-"""new = np.array([[1,2,3]])
-new2= np.array([[4,5,6]])
-print np.concatenate((new , np.subtract(new2,new)),axis=0)
-new = np.concatenate((new , np.subtract(new2,new)),axis=0)
-print new
-#print curve
-#for i in curve:
- #   print "\n",i
+    gradi1 = grad(curve, scale1)
+   # gradi2 = grad(curve, scale2)
+   # gradi3 = grad(curve, scale3)
+    len1 = length(gradi1, scale1)
+    #len2 = length(gradi2, scale2)
+    #len3 = length(gradi3, scale3)
+    len1 = np.delete(len1, curve.shape[0] - 2 * scale1, 0)
+    #print "\n\ntest1:: \n\n",len1
+    #len2 = np.delete(len2, curve.shape[0] - 2 * scale2, 0)
+    #len3 = np.delete(len3, curve.shape[0] - 2 * scale3, 0)
+    zip(*len1)
+    #zip(*len2)
+    #zip(*len3)
+    plt.figure()
+    plt.subplot(2,1,a)
+    if ok in ('t', 'T'):
+        plt.title('Truepath(Nicht normiert)')
+    elif ok in ('f', 'F'):
+        plt.title('Falsepath(Nicht normiert)')
+    a1=plt.scatter(*zip(*len1), color="red")
+    #a2=plt.scatter(*zip(*len2), color="blue")
+    #a3=plt.scatter(*zip(*len3), color="green")
+    #plt.legend((a1,a2,a3),(scale1, scale2, scale3),scatterpoints=1,loc='lower left',ncol=3,fontsize=12)
 
-#curvetest=np.array((curve[0],curve[1]))
-#print "\n","curvetest:","\n", curvetest
-#b=np.array([[1, 2, 3]])
-#curvetest2=np.concatenate((curvetest,b),axis=0)
-#print "\n","curvetest2:","\n", curvetest2
-#print "\n","Kurve:","\n",np.array((curve[0],curve[1]))
-f.close()
-#print "\n","Array:","\n", np.array([[1, 2, 6], [3, 4, 5]])
+    len1 = [(elem1, elem2 / scale1) for elem1, elem2 in len1]
+    #len2 = [(elem1, elem2 / scale2) for elem1, elem2 in len2]
+    #len3 = [(elem1, elem2 / scale3) for elem1, elem2 in len3]
 
-#gradient= np.gradient(curve,1)
-#print "\n",gradient
 
+    zip(*len1)
+    #zip(*len2)
+    #zip(*len3)
+
+    plt.subplot(2,1,b)
+    if ok in ('t', 'T'):
+        plt.title('Truepath(Normiert)')
+    elif ok in ('f', 'F'):
+        plt.title('Falsepath(Normiert)')
+    b1=plt.scatter(*zip(*len1), color="red")
+    #b2=plt.scatter(*zip(*len2), color="blue")
+    #b3=plt.scatter(*zip(*len3), color="green")
+    #plt.legend((b1, b2, b3), (scale1, scale2, scale3) ,scatterpoints=1,loc='lower left',ncol=3,fontsize=12)
+    return len1
+
+#f = h5py.File("/home/axeleik/Downloads/cremi.paths.crop.split_z.h5", mode='r')
+#curve1 = np.array(f["z_predict0/truepaths/z/0/beta_0.5/11/0"])[:,:]
 """
 
+scale1 = int(input("please enter scale 1: "))
+scale2 = int(input("please enter scale 2: "))
+scale3 = int(input("please enter scale 3: "))
+
+curve1 = np.array(f["z_predict0/truepaths/z/0/beta_0.5/11/0"])[:,:]
+curve2 = np.array(f["z_predict0/falsepaths/z/0/beta_0.5/247/0"])[:,:]
+curve3 = np.array(f["z_predict0/falsepaths/z/0/beta_0.5/247/0"])[:,:]
+curve4 = np.array(f["z_predict0/falsepaths/z/0/beta_0.5/259/0"])[:,:]
+curve5 = np.array(f["z_predict0/falsepaths/z/0/beta_0.5/260/0"])[:,:]
+f.close()
+plt.figure()
+
+plot_tf(curve1,"t",scale1,scale2,scale3,1,2)
+plot_tf(curve2,"f",scale1,scale2,scale3,3,4)
+plot_tf(curve3,"t",scale1,scale2,scale3,5,6)
+plot_tf(curve4,"f",scale1,scale2,scale3,7,8)
+plot_tf(curve5,"f",scale1,scale2,scale3,9,10)
+
+plt.show()
 
 
 
-
+f.visit(printname)
+"""
 
---
+++ interpolation.py
@@ -0,0 +1,65 @@
+
+
+import h5py
+import scipy
+from scipy import interpolate
+import numpy as np
+import curvature
+
+f = h5py.File("/home/axeleik/Downloads/cremi.paths.crop.split_z.h5", mode='r')
+data = np.array(f["z_predict0/truepaths/z/0/beta_0.5/247/0"])[:,:]
+data2 = np.array(f["z_predict0/falsepaths/z/0/beta_0.5/247/0"])[:,:]
+
+data = np.array([(elem1, elem2,elem3*10) for elem1, elem2,elem3 in data])
+#data2 = np.array([(elem1, elem2,elem3*10) for elem1, elem2,elem3 in data2])
+#This is your data, but we're 'zooming' into just 5 data points
+#because it'll provide a better visually illustration
+#also we need to transpose to get the data in the right format
+data = data.transpose()
+#data2 = data2.transpose()
+
+#now we get all the knots and info about the interpolated spline
+tck, u= interpolate.splprep(data)
+#tck2, u2= interpolate.splprep(data2)
+#here we generate the new interpolated dataset,
+#increase the resolution by increasing the spacing, 500 in this example
+new = interpolate.splev(np.linspace(0,1,500), tck)
+print len(new)
+#new2 = interpolate.splev(np.linspace(0,1,500), tck2)
+
+#now lets plot it!
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+fig = plt.figure(1)
+ax = Axes3D(fig)
+ax.plot(data[0], data[1], data[2], label='original_true', lw =2, c='Dodgerblue')
+#ax.plot(data2[0], data2[1], data2[2], label='original_false', lw =2, c='Green')
+ax.plot(new[0], new[1], new[2], label='fit_true', lw =3, c='red')
+#ax.plot(new2[0], new2[1], new2[2], label='fit_false', lw =2, c='Yellow')
+#plt.savefig('/home/axeleik/Downloads/junk.png')
+
+x=int(input("Skala: "))
+curves=curvature.plot_tf(np.array(new).transpose(),"t",x,10,20,1,2)
+
+
+small = np.array([(elem1, elem2) for (elem1, elem2) in curves if elem2<6.0])
+print "small: ", small
+orte=[elem1 for (elem1, elem2) in small]
+print "orte: ", orte
+print len(orte)
+new_new= np.array(new).transpose()
+#print "\n\nnew_new",new_new.transpose()
+print "new_new.shape:",new_new.shape
+
+small_array = np.array([elem1 for elem1 in np.array(new_new)[orte]])
+print "small_array: ", small_array
+print len(small_array)
+new_new=small_array.transpose()
+print "new_new:",new_new
+print len(new_new[1])
+#ax.plot(new_new[0], new_new[1], new_new[2], label='small', lw =2, c='yellow')
+
+ax.legend()
+
+
+plt.show()
